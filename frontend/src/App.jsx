008c6a6c166593adc5ebb0725f03efd2c3177b5c
Adrian011027 <angel10000sv@gmail.com>
2026-06-15 09:17:32 -0600

    refactor: directorio frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import FormularioQR from "./components/FormularioQR";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+
+      <FormularioQR />
+    </div>
+  );
+}
+
+export default App;
+
+
